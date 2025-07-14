1903c9edd2ef36d16e9e87cfa915f35ca2253096
Law ence <fedexexpresssservice0@gmail.com>
2025-07-14 08:42:59 +0100

    Updated User schema default role to admin

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const TrackingSchema = new mongoose.Schema({
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' }
+    role: { type: String, enum: ['user', 'admin'], default: 'admin' }
 });
 
 // Hash password before saving
